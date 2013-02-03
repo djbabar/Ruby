81fdaefa67a489e61e39d0c059d9c45ada0a1cca
Mark Winter <mwinter@gmail.com>
2013-02-03 11:38:13 -0800

    text file with logging

--- wk6/ex3.rb
+++ wk6/ex3.rb
@@ -2,27 +2,40 @@
   modify text analyzer to use logging feature
 =end
 
-def text_analyzer(file='text.txt')
+require 'logger'
 
-  lines = File.readlines(file)
-  line_count = lines.length
-  text = lines.join
-  character_count = text.length
-  character_count_without_spaces = text.gsub(/\s+/, '').length
-  word_count = text.split(" ").length
-  sentence_count = text.split(/\.|\?|!/).length
-  paragraph_count = text.split(/\n\n/).length
-  ave_num_words_per_sentence = word_count / sentence_count
-  ave_num_sentences_per_paragraph = sentence_count / paragraph_count
+class File_analyzer
+  attr_reader  :lines, :character_count, :character_count_without_spaces, :line_count, :word_count, :sentence_count, :paragraph_count, :avg_num_words_per_sentence, :avg_num_sentences_per_paragraph
+  def initialize(file='text.txt')
+    @lines = File.readlines(file)
+    $LOG.info("Reading file #{file}")
+    file_stats
+    $LOG.info("Statistics collected for #{file}")
+  end
 
-  puts "Character count: #{character_count}"
-  puts "Character count (excluding spaces): #{character_count_without_spaces}"
-  puts "Line count: #{line_count}"
-  puts "Word count: #{word_count}"
-  puts "Sentence count: #{sentence_count}"
-  puts "Paragraph count: #{paragraph_count}"
-  puts "Average number of words per sentence: #{ave_num_words_per_sentence}"
-  puts "Average number of sentences per paragraph: #{ave_num_sentences_per_paragraph}"
+  def file_stats
+    @line_count = @lines.length
+    @text = @lines.join
+    @character_count = @text.length
+    @character_count_without_spaces = @text.gsub(/\s+/, '').length
+    @word_count = @text.split(" ").length
+    @sentence_count = @text.split(/\.|\?|!/).length
+    @paragraph_count = @text.split(/\n\n/).length
+    @avg_num_words_per_sentence = @word_count / sentence_count
+    @avg_num_sentences_per_paragraph = @sentence_count / paragraph_count
+  end
 end
 
-text_analyzer(*ARGV)
+
+$LOG = Logger.new('fileread.log')
+file = File_analyzer.new(*ARGV)
+
+puts "Character count: #{file.character_count}"
+puts "Character count (excluding spaces): #{file.character_count_without_spaces}"
+puts "Line count: #{file.line_count}"
+puts "Word count: #{file.word_count}"
+puts "Sentence count: #{file.sentence_count}"
+puts "Paragraph count: #{file.paragraph_count}"
+puts "Average number of words per sentence: #{file.avg_num_words_per_sentence}"
+puts "Average number of sentences per paragraph: #{file.avg_num_sentences_per_paragraph}"
+
